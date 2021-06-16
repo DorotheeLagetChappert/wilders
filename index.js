b4f9747d1805cf3484ece3793687f28e5e640532
Dorothée Laget-Chappert <dorothee.lagetchappert@gmail.com>
2021-06-16 15:58:55 +0200

    mywilders create

---
+++ index.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk');
+const log = console.log;
+
+log(chalk.white('Hello ') + chalk.rgb(123, 45, 67)('Ludivine,') + chalk.red(' Tristan,') + chalk.blue(' Rémy ') + chalk.white('et')+ chalk.green(' Benoît') + chalk.white('.'));
+
+
+
